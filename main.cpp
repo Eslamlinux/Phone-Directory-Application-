669a0a78c0f950f3ebd096ea09bbd1f2bb9fe37f
Eslam Linux <linuxeslam@gmail.com>
2025-08-24 02:22:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -56,14 +56,24 @@ void setup(){
 }
 
 void addNumber(){
-    string entry;
+    string entry_name;
+    string entry_num;
     fstream myData;
-    cout << "enter your new content\n";
-    getline(cin,entry);
+    cout << "enter your new content name\n";
+    getline(cin,entry_name);
+    cout << "enter your new content number must be less than 18 number only\n";
+    cin >> entry_num;
+    if(entry_num.size() > 18){
+     cout << "you entered more than 18 number try agin\n";
+     cin.clear();
+     cin.ignore();
+     addNumber();
+     return;
+    }
     myData.open("myPhoneData.txt" , ios::out|ios::app );
 
     if(myData.is_open()){
-    myData << entry << endl;
+    myData << entry_name << " | " << entry_num << endl;
     myData.close();
     }
     else{
