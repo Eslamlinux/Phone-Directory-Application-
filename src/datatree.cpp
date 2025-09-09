54c1cdd7a3b979a09e674ed51e225d67ba23aef7
Eslam Linux <linuxeslam@gmail.com>
2025-09-09 22:02:15 +0000

    updates and working on tree

--- src/datatree.cpp
+++ src/datatree.cpp
@@ -22,36 +22,35 @@ class filetree{
     filetree(){
         root = NULL;
     }
-    bool isEmpty(){
-        return root == NULL;
+    bool isEmpty(Node* r){
+        return r == NULL;
     }
-    void To_insert(Node* r,std::string val){
+    Node* To_insert(Node* r,std::string val){
         Node *newNode = new Node(val);
-        if(isEmpty()){
-            root = newNode;
+        if(isEmpty(r)){
+            r = newNode;
+        }
+        else if(val < r->data){
+            r->left = To_insert(r->left,val);
+        }
+        else if(val > r->data){
+            r->right = To_insert(r->right,val);
+        }
+        return r;
+                }
+
+    void insert(std::string val){
+        root = To_insert(root,val);
+    }
+    void print_inorder(Node* r){
+        if(r == NULL){
+            return;
         }
         else{
-            if(val < r->data){
-                if(r->left == NULL){
-                    r->left = newNode;
-                }
-                else{
-                    To_insert(r->left,val);
-                }
-            }
-            else if(val > r->data){
-                if(r->right == NULL){
-                    r->right = newNode;
-                }
-                else{
-                    To_insert(r->right,val);
-                }
-            }
-            To_insert(root, val);
+            print_inorder(r->left);
+            std::cout << r->data << " ";
+            print_inorder(r->right);
         }
-    }
-    void insert(Node* r, std::string val){
-        To_insert(root,val);
     }
     
 };
