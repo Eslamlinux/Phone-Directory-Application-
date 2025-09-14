80cfbd130376119ba68f979f0212bac966e570d9
Eslam Linux <linuxeslam@gmail.com>
2025-09-14 18:03:50 +0000

    update

---
+++ build/CMakeFiles/phone_directory.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phone_directory.dir/link.d"
+  "CMakeFiles/phone_directory.dir/main.cpp.o"
+  "CMakeFiles/phone_directory.dir/main.cpp.o.d"
+  "CMakeFiles/phone_directory.dir/src/The_counter.cpp.o"
+  "CMakeFiles/phone_directory.dir/src/The_counter.cpp.o.d"
+  "CMakeFiles/phone_directory.dir/src/addNumber.cpp.o"
+  "CMakeFiles/phone_directory.dir/src/addNumber.cpp.o.d"
+  "CMakeFiles/phone_directory.dir/src/datatree.cpp.o"
+  "CMakeFiles/phone_directory.dir/src/datatree.cpp.o.d"
+  "CMakeFiles/phone_directory.dir/src/deleteAllContacts.cpp.o"
+  "CMakeFiles/phone_directory.dir/src/deleteAllContacts.cpp.o.d"
+  "CMakeFiles/phone_directory.dir/src/readList.cpp.o"
+  "CMakeFiles/phone_directory.dir/src/readList.cpp.o.d"
+  "CMakeFiles/phone_directory.dir/src/setup.cpp.o"
+  "CMakeFiles/phone_directory.dir/src/setup.cpp.o.d"
+  "CMakeFiles/phone_directory.dir/src/utils.cpp.o"
+  "CMakeFiles/phone_directory.dir/src/utils.cpp.o.d"
+  "phone_directory"
+  "phone_directory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/phone_directory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
